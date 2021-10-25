a7e1116f28fbdf08ba89fbcf4f55734560fda935
poojan2021 <79490007+poojan2021@users.noreply.github.com>
2021-10-25 10:58:49 -0400

    Add files via upload

--- ngCheckers.js
+++ ngCheckers.js
@@ -57,6 +57,144 @@ angular.module('ngCheckers', [])
 				}
 			}
 		}
+		
+		$scope.select = function(square) {
+			if(selectedSquare !== null && !square.player){
+				movePiece(square);
+				resetChoices();
+			} else if (square.player === $scope.player){
+				selectedSquare = square;
+				resetChoices();
+				setChoices(selectedSquare.x, selectedSquare.y, 1, [], -1, -1);
+			} else {
+				selectedSquare = null;
+			}
+			console.log($scope.board);
+		}
+		
+		function resetChoices(){
+			for(var i = 0; i < BOARD_WIDTH; i++) {
+				for(var j = 0; j < BOARD_WIDTH; j++) {
+					$scope.board[i][j].isChoice = false;
+					$scope.board[i][j].matados = [];
+				}
+			}
+		}
+		
+		function movePiece(square) {
+			if(square.isChoice) {
+				square.player = selectedSquare.player;
+				
+				selectedSquare.player = null;
+				
+				$scope.player = $scope.player === RED ? BLACK : RED;
+			}
+		}
+		
+		function setChoices(x, y, depth, matados, oldX, oldY, isKing) {
+			if(depth > 10) return;
+			isKing = isKing ||
+			($scope.player === RED && y==0);
+			($scope.player === BLACK && y == BOARD_WIDTH - 1);
+			if($scope.player === RED || isKing) {
+				//check left side, a square might not be available
+				//Upper Left
+				if(x > 0 && y > 0) {
+					var UP_LEFT = $scope.board[y-1][x-1];
+					if(UP_LEFT.player) {
+						if(UP_LEFT.player !== $scope.player) {
+							if((x > 1 && y > 1) && !(x - 2 === oldX && y -2 === oldY)) {
+								var UP_LEFT_2 = $scope.board[y-2][x-2];
+								if(!UP_LEFT_2.player) {
+									UP_LEFT_2.isChoice = true;
+									var jumpers = matados.slice(0);
+									if(jumpers.indexOf(UP_LEFT) === -1)
+										jumpers.push(UP_LEFT);
+									UP_LEFT_2.matados = jumpers;
+									setChoices(x-2, y-2,depth+1,jumpers,x,y, isKing);
+								}
+							}
+						}
+					}else if (depth === 1) {
+						UP_LEFT.isChoice = true;
+					}
+				}
+				
+				//Upper Right
+				if( x < BOARD_WIDTH - 1 && y > 0) {
+					var UP_RIGHT = $scope.board[y-1][x+1];
+					if(UP_RIGHT.player) {
+						if(UP_RIGHT.player !== $scope.player) {
+							if((x < BOARD_WIDTH - 2 && y > 1) && !(x + 2 === oldX && y - 2 === oldY)) {
+								var UP_RIGHT_2 = $scope.board[y-2][x+2];
+								if(!UP_RIGHT_2.player) {
+									UP_RIGHT_2.isChoice = true;
+									var jumpers = matados.slice(0);
+									if (jumpers.indexOf(UP_RIGHT) === -1)
+										jumpers.push(UP_RIGHT);
+									UP_RIGHT_2.matados = jumpers;
+									setChoices(x+2, y-2, depth+1, jumpers, x, y, isKing);
+								}
+							}
+						}
+					}else if (depth === 1) {
+						UP_RIGHT.isChoice = true;
+					}
+				}
+			}
+			
+			//Lower Choices
+			if($scope.player === BLACK || isKing) {
+				//Lower Left
+				if(x > 0 && y < BOARD_WIDTH - 1) {
+					var LOWER_LEFT = $scope.board[y+1][x-1];
+					if(LOWER_LEFT.player) {
+						if(LOWER_LEFT.player !== $scope.player) {
+							if((x > 1 && y < BOARD_WIDTH - 2) && !(x-2 === oldX && y + 2 === oldY)) {
+								var LOWER_LEFT_2 = $scope.board[y+2][x-2];
+								if(!LOWER_LEFT_2.player){
+									LOWER_LEFT_2.isChoice = true;
+									var jumpers = matados.slice(0);
+									if(jumpers.indexOf(LOWER_LEFT) === -1)
+										jumpers.push(LOWER_LEFT);
+									LOWER_LEFT_2.matados = jumpers;
+									setChoices(x-2, y+2, depth+1, jumpers, x, y, isKing);
+								}
+							}
+						}
+					}else if (depth === 1) {
+						LOWER_LEFT.isChoice = true;
+					}
+				}
+				
+				//Lower Right
+				if (x < BOARD_WIDTH - 1 && y < BOARD_WIDTH - 1) {
+					var LOWER_RIGHT = $scope.board[y+1][x+1];
+					if(LOWER_RIGHT.player) {
+						if(LOWER_RIGHT.player != $scope.player) {
+							if((x < BOARD_WIDTH - 2 && y < BOARD_WIDTH - 2) && !(x + 2 === oldX && y + 2 === oldY)) {
+								var LOWER_RIGHT_2 = $scope.board[y+2][x+2];
+								if(!LOWER_RIGHT_2.player){
+									LOWER_RIGHT_2.isChoice = true;
+									var jumpers = matados.slice(0);
+									if(jumpers.indexOf(LOWER_RIGHT) === 1)
+										jumpers.push(LOWER_RIGHT);
+									LOWER_RIGHT_2.matados = jumpers;
+									setChoices(x+2, y+2, depth+1, jumpers, x, y, isKing);
+								}
+							}
+						}
+					}else if (depth === 1) {
+						LOWER_RIGHT.isChoice = true;
+					}
+				}
+			}
+		}
+							
+					
+			
+			
+			
 	});
 			
 			
